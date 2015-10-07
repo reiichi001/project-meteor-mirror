af2262c4b1508cfbb72dad8896e72de9b1f9ecfc
Filip Maj <filipmaj@gmail.com>
2015-10-06 20:06:08 -0400

    Added chat packet and state change packet. Fixed file spelling error for RemoveActorPacket and deleted UnknownLoginPacket. Was empty.

---
+++ FFXIVClassic Map Server/packets/send/Actor/SetActorStatePacket.cs
@@ -0,0 +1,27 @@
+﻿using FFXIVClassic_Lobby_Server.packets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.send.actor
+{
+    class SetActorStatePacket
+    {
+        public const ushort OPCODE = 0x0134;
+        public const uint PACKET_SIZE = 0x28;
+
+        public const int STATE_NONE     = 0x0000;
+		public const int STATE_DEAD		= 0x0303;
+		public const int STATE_PASSIVE	= 0xBF00;
+		public const int STATE_ACTIVE	= 0xBF02;
+
+        public static SubPacket buildPacket(uint playerActorID, uint targetID, uint state)
+        {
+            byte[] data = new byte[PACKET_SIZE - 0x20];
+
+            return new SubPacket(OPCODE, playerActorID, targetID, data);
+        }
+    }
+}
