2f6841b12581914ce9fba43c992247f0090de2cf
Filip Maj <filipmaj@gmail.com>
2015-10-12 10:45:35 -0400

    Fixed emote not working when no target was set.

--- FFXIVClassic Map Server/packets/send/Actor/ActorDoEmotePacket.cs
+++ FFXIVClassic Map Server/packets/send/Actor/ActorDoEmotePacket.cs
@@ -17,12 +17,15 @@ namespace FFXIVClassic_Map_Server.packets.send.actor
         {
             byte[] data = new byte[PACKET_SIZE - 0x20];
 
+            if (targetActorID == 0xC0000000)
+                targetActorID = playerActorID;
+
             using (MemoryStream mem = new MemoryStream(data))
             {
                 using (BinaryWriter binWriter = new BinaryWriter(mem))
                 {
                     uint realAnimID = 0x5000000 | ((emoteID - 100) << 12);
-                    uint realDescID = 20000 + ((emoteID - 1) * 10) + (targetActorID == playerActorID || targetActorID == 0xC0000000 ? (uint)2 : (uint)1);
+                    uint realDescID = 20000 + ((emoteID - 1) * 10) + (targetActorID == playerActorID ? (uint)2 : (uint)1);
                     binWriter.Write((UInt32)realAnimID);
                     binWriter.Write((UInt32)targetActorID);
                     binWriter.Write((UInt32)realDescID);
