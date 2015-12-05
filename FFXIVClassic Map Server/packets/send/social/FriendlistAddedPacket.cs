bda686681d6081eaaac81219ca3d449308ad34c4
Filip Maj <filipmaj@gmail.com>
2015-12-04 23:39:39 -0500

    I've begun working on implementing social packets.

---
+++ FFXIVClassic Map Server/packets/send/social/FriendlistAddedPacket.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.send.social
+{
+    class FriendlistAddedPacket
+    {
+    }
+}
