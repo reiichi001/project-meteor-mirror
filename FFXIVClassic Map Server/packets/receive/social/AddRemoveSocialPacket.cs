daaded83c368010b718b85422cebe7e55e5f9a2c
Filip Maj <filipmaj@gmail.com>
2015-12-05 00:11:29 -0500

    Added receive packet for all social add/remove. They are all the same structure so a single packet should do.

---
+++ FFXIVClassic Map Server/packets/receive/social/AddRemoveSocialPacket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.receive.social
+{
+    class AddRemoveSocialPacket
+    {
+        public bool invalidPacket = false;
+        public string name;
+
+        public AddRemoveSocialPacket(byte[] data)
+        {
+            using (MemoryStream mem = new MemoryStream(data))
+            {
+                using (BinaryReader binReader = new BinaryReader(mem))
+                {
+                    try{
+                        name = Encoding.ASCII.GetString(binReader.ReadBytes(0x20));
+                    }
+                    catch (Exception){
+                        invalidPacket = true;
+                    }
+                }
+            }
+        }
+    }
+}
