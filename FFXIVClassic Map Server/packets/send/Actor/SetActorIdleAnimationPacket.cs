93a42c1ba3bd205b16690db6c8fe0659f40cf0fc
Filip Maj <filipmaj@gmail.com>
2016-01-02 18:16:38 -0500

    Implemented the SetIdleAnimation packet, and a placeholder for packet 0x132 used to login.

---
+++ FFXIVClassic Map Server/packets/send/Actor/SetActorIdleAnimationPacket.cs
@@ -0,0 +1,32 @@
+﻿using FFXIVClassic_Lobby_Server.packets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.send.actor
+{
+    class SetActorIdleAnimationPacket
+    {        
+        public const ushort OPCODE = 0x144;
+        public const uint PACKET_SIZE = 0x28;
+
+        public static SubPacket buildPacket(uint playerActorID, uint targetID, uint idleAnimationId)
+        {
+            byte[] data = new byte[PACKET_SIZE - 0x20];
+
+            using (MemoryStream mem = new MemoryStream(data))
+            {
+                using (BinaryWriter binWriter = new BinaryWriter(mem))
+                {
+                   binWriter.Seek(0x6, SeekOrigin.Begin);
+                   binWriter.Write((UInt16)(idleAnimationId&0xFFFF));
+                }
+            }
+
+            return new SubPacket(OPCODE, playerActorID, targetID, data);
+        }
+    }
+}
