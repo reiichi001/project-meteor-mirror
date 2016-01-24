3b48ed1f742d4f3345ad49a0f9bf3f8b2a3dc906
Filip Maj <filipmaj@gmail.com>
2016-01-23 20:12:04 -0500

    Added actor event packets.

---
+++ FFXIVClassic Map Server/packets/send/Actor/events/SetEmoteEventCondition.cs
@@ -0,0 +1,36 @@
+﻿using FFXIVClassic_Lobby_Server.packets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.send.actor.events
+{
+    class SetEmoteEventCondition
+    {
+        public const ushort OPCODE = 0x016C;
+        public const uint PACKET_SIZE = 0x48;
+
+        public static SubPacket buildPacket(uint playerActorID, uint targetActorID)
+        {
+            byte[] data = new byte[PACKET_SIZE - 0x20];
+
+            string eventName = "";
+
+            using (MemoryStream mem = new MemoryStream(data))
+            {
+                using (BinaryWriter binWriter = new BinaryWriter(mem))
+                {                    
+                    binWriter.Write((Byte)0);
+                    binWriter.Write((Byte)0);
+                    binWriter.Write((UInt16)0);
+                    binWriter.Write(Encoding.ASCII.GetBytes(eventName), 0, Encoding.ASCII.GetByteCount(eventName) >= 0x24 ? 0x24 : Encoding.ASCII.GetByteCount(eventName));
+                }
+            }
+
+            return new SubPacket(OPCODE, playerActorID, playerActorID, data);
+        }
+    }
+}
