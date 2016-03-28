f8ab0cd86de6e916d6a8c4243af38dea101f728a
Filip Maj <filipmaj@gmail.com>
2016-03-28 11:30:02 -0400

    Added langauge code packet (0x6) and 0x2/0x7 temp packets to clean up the login packets sent by client.

---
+++ FFXIVClassic Map Server/packets/send/_0x02Packet.cs
@@ -0,0 +1,32 @@
+﻿using FFXIVClassic_Lobby_Server.packets;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.send
+{
+    class _0x02Packet
+    {
+        public const ushort OPCODE = 0x0002;
+        public const uint PACKET_SIZE = 0x30;
+
+        public static SubPacket buildPacket(uint playerActorId, int val)
+        {
+            byte[] data = new byte[PACKET_SIZE - 0x20];
+
+            using (MemoryStream mem = new MemoryStream(data))
+            {
+                using (BinaryWriter binWriter = new BinaryWriter(mem))
+                {
+                    binWriter.Seek(8, SeekOrigin.Begin);
+                    binWriter.Write((UInt32)playerActorId);
+                }
+            }
+
+            return new SubPacket(OPCODE, playerActorId, playerActorId, data);
+        }
+    }
+}
