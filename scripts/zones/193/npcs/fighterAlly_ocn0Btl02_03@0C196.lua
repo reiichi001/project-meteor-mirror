ffae2b744f09615c7a46779fe62fba565520a5e3
Filip Maj <filipmaj@gmail.com>
2016-04-18 13:35:10 -0400

    Untracked the ./scripts folder since it was moved to ./data/scripts

--- scripts/zones/193/npcs/fighterAlly_ocn0Btl02_03@0C196.lua
+++
@@ -1,3 +0,0 @@
-function init(npc)
-	return "/Chara/Npc/Monster/Fighter/FighterAllyOpeningAttacker", false, false, false, false, false, npc.getActorClassId(), false, false, 10, 1, 4, false, false, false, false, false, false, false, false, 2;
-end
