0aac675b30e6a61e7f82385aae7923721cd30afe
Tahir Akhlaq <xdemolish@hotmail.com>
2016-06-20 19:21:19 +0100

    cleaned up previous commit

--- FFXIVClassic Common Class Lib/Sql.cs
+++ FFXIVClassic Common Class Lib/Sql.cs
@@ -5,76 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using NLog;
+using System.Data;
+using System.Data.Common;
 
 namespace FFXIVClassic.Common
 {
-    /*
-    class SqlCommand
+    // todo:
+    // havent decided whether it's worth wrapping every sql class
+    // so i'll just leave it with logger for now
+    public class Sql
     {
         public static Logger Log = LogManager.GetCurrentClassLogger();
-
-        public SqlCommand()
-        {
-
-        }
-
-        public SqlCommand(string cmdText)
-        {
-            try
-            {
-                MySqlCommand.MySqlCommand("");
-            }
-        }
-        public SqlCommand(string cmdText, MySqlConnection connection);
-        public SqlCommand(string cmdText, MySqlConnection connection, MySqlTransaction transaction);
-
-        ~SqlCommand()
-        {
-
-        }
-
-        public int CacheAge { get; set; }
-        
-        public string CommandText { get; set; }
-        public int CommandTimeout { get; set; }
-        
-        public CommandType CommandType { get; set; }
-
-        public MySqlConnection Connection { get; set; }
-        
-        public bool DesignTimeVisible { get; set; }
-        public bool EnableCaching { get; set; }
-        
-        public bool IsPrepared { get; }
-        
-        public long LastInsertedId { get; }
-
-        public MySqlParameterCollection Parameters { get; }
-        
-        public MySqlTransaction Transaction { get; set; }
-        public UpdateRowSource UpdatedRowSource { get; set; }
-        protected DbConnection DbConnection { get; set; }
-        protected DbParameterCollection DbParameterCollection { get; }
-        protected DbTransaction DbTransaction { get; set; }
-
-        public IAsyncResult BeginExecuteNonQuery();
-        public IAsyncResult BeginExecuteNonQuery(AsyncCallback callback, object stateObject);
-        public IAsyncResult BeginExecuteReader();
-        public IAsyncResult BeginExecuteReader(CommandBehavior behavior);
-        public void Cancel();
-        public SqlCommand Clone();
-        public MySqlParameter CreateParameter();
-        public void Dispose();
-        public int EndExecuteNonQuery(IAsyncResult asyncResult);
-        public MySqlDataReader EndExecuteReader(IAsyncResult result);
-        public int ExecuteNonQuery();
-        public MySqlDataReader ExecuteReader();
-        public MySqlDataReader ExecuteReader(CommandBehavior behavior);
-        public object ExecuteScalar();
-        public void Prepare();
-        protected DbParameter CreateDbParameter();
-        protected void Dispose(bool disposing);
-        protected DbDataReader ExecuteDbDataReader(CommandBehavior behavior);
     }
-    */
 }
