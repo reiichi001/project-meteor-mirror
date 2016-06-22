b8a563f9d7d59bd42a0b8d8b51891a4216a70815
deviltti <deviltti@gmail.com>
2016-06-21 21:13:01 -0400

    Lobby and game server packet ConsoleColor to NLog cleanup.

--- FFXIVClassic Common Class Lib/Utils.cs
+++ FFXIVClassic Common Class Lib/Utils.cs
@@ -10,10 +10,10 @@ namespace FFXIVClassic.Common
         private static uint[] CreateLookup32()
         {
             var result = new uint[256];
-            for (int i = 0; i < 256; i++)
+            for (var i = 0; i < 256; i++)
             {
-                string s = i.ToString("X2");
-                result[i] = ((uint)s[0]) + ((uint)s[1] << 16);
+                var s = i.ToString("X2");
+                result[i] = s[0] + ((uint) s[1] << 16);
             }
             return result;
         }
@@ -22,25 +22,23 @@ namespace FFXIVClassic.Common
         {
             if (bytes == null)
             {
-                return String.Empty;
+                return string.Empty;
             }
 
-            char[] hexChars = "0123456789ABCDEF".ToCharArray();
+            var hexChars = "0123456789ABCDEF".ToCharArray();
 
-            // 00000000   00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  ................
-            // 00000010   00 00 00 00 00 00 00 00  00 00 00 00 00 00 00 00  ................
-            int offsetBlock = 8 + 3;
-            int byteBlock = offsetBlock + (bytesPerLine * 3) + ((bytesPerLine - 1) / 8) + 2;
-            int lineLength = byteBlock + bytesPerLine + Environment.NewLine.Length;
+            var offsetBlock = 8 + 3;
+            var byteBlock = offsetBlock + bytesPerLine*3 + (bytesPerLine - 1)/8 + 2;
+            var lineLength = byteBlock + bytesPerLine + Environment.NewLine.Length;
 
-            char[] line = (new String(' ', lineLength - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
-            int numLines = (bytes.Length + bytesPerLine - 1) / bytesPerLine;
+            var line = (new string(' ', lineLength - Environment.NewLine.Length) + Environment.NewLine).ToCharArray();
+            var numLines = (bytes.Length + bytesPerLine - 1)/bytesPerLine;
 
-            StringBuilder sb = new StringBuilder(numLines * lineLength);
+            var sb = new StringBuilder(numLines*lineLength);
 
-            for (int i = 0; i < bytes.Length; i += bytesPerLine)
+            for (var i = 0; i < bytes.Length; i += bytesPerLine)
             {
-                int h = i + offset;
+                var h = i + offset;
 
                 line[0] = hexChars[(h >> 28) & 0xF];
                 line[1] = hexChars[(h >> 24) & 0xF];
@@ -51,10 +49,10 @@ namespace FFXIVClassic.Common
                 line[6] = hexChars[(h >> 4) & 0xF];
                 line[7] = hexChars[(h >> 0) & 0xF];
 
-                int hexColumn = offsetBlock;
-                int charColumn = byteBlock;
+                var hexColumn = offsetBlock;
+                var charColumn = byteBlock;
 
-                for (int j = 0; j < bytesPerLine; j++)
+                for (var j = 0; j < bytesPerLine; j++)
                 {
                     if (j > 0 && (j & 7) == 0)
                     {
@@ -69,10 +67,10 @@ namespace FFXIVClassic.Common
                     }
                     else
                     {
-                        byte by = bytes[i + j];
+                        var by = bytes[i + j];
                         line[hexColumn] = hexChars[(by >> 4) & 0xF];
                         line[hexColumn + 1] = hexChars[by & 0xF];
-                        line[charColumn] = (by < 32 ? '.' : (char)by);
+                        line[charColumn] = by < 32 ? '.' : (char) by;
                     }
 
                     hexColumn += 3;
@@ -82,60 +80,60 @@ namespace FFXIVClassic.Common
                 sb.Append(line);
             }
 
-            return sb.ToString();
+            return sb.ToString().TrimEnd(Environment.NewLine.ToCharArray());
         }
 
-        public static UInt32 UnixTimeStampUTC()
+        public static uint UnixTimeStampUTC()
         {
-            UInt32 unixTimeStamp;
-            DateTime currentTime = DateTime.Now;
-            DateTime zuluTime = currentTime.ToUniversalTime();
-            DateTime unixEpoch = new DateTime(1970, 1, 1);
-            unixTimeStamp = (UInt32)(zuluTime.Subtract(unixEpoch)).TotalSeconds;
+            uint unixTimeStamp;
+            var currentTime = DateTime.Now;
+            var zuluTime = currentTime.ToUniversalTime();
+            var unixEpoch = new DateTime(1970, 1, 1);
+            unixTimeStamp = (uint) zuluTime.Subtract(unixEpoch).TotalSeconds;
 
             return unixTimeStamp;
         }
 
-        public static UInt64 MilisUnixTimeStampUTC()
+        public static ulong MilisUnixTimeStampUTC()
         {
-            UInt64 unixTimeStamp;
-            DateTime currentTime = DateTime.Now;
-            DateTime zuluTime = currentTime.ToUniversalTime();
-            DateTime unixEpoch = new DateTime(1970, 1, 1);
-            unixTimeStamp = (UInt64)(zuluTime.Subtract(unixEpoch)).TotalMilliseconds;
+            ulong unixTimeStamp;
+            var currentTime = DateTime.Now;
+            var zuluTime = currentTime.ToUniversalTime();
+            var unixEpoch = new DateTime(1970, 1, 1);
+            unixTimeStamp = (ulong) zuluTime.Subtract(unixEpoch).TotalMilliseconds;
 
             return unixTimeStamp;
         }
 
         public static ulong SwapEndian(ulong input)
         {
-            return ((0x00000000000000FF) & (input >> 56) |
-                    (0x000000000000FF00) & (input >> 40) |
-                    (0x0000000000FF0000) & (input >> 24) |
-                    (0x00000000FF000000) & (input >> 8) |
-                    (0x000000FF00000000) & (input << 8) |
-                    (0x0000FF0000000000) & (input << 24) |
-                    (0x00FF000000000000) & (input << 40) |
-                    (0xFF00000000000000) & (input << 56));
+            return 0x00000000000000FF & (input >> 56) |
+                   0x000000000000FF00 & (input >> 40) |
+                   0x0000000000FF0000 & (input >> 24) |
+                   0x00000000FF000000 & (input >> 8) |
+                   0x000000FF00000000 & (input << 8) |
+                   0x0000FF0000000000 & (input << 24) |
+                   0x00FF000000000000 & (input << 40) |
+                   0xFF00000000000000 & (input << 56);
         }
 
         public static uint SwapEndian(uint input)
         {
             return ((input >> 24) & 0xff) |
-                    ((input << 8) & 0xff0000) |
-                    ((input >> 8) & 0xff00) |
-                    ((input << 24) & 0xff000000);
+                   ((input << 8) & 0xff0000) |
+                   ((input >> 8) & 0xff00) |
+                   ((input << 24) & 0xff000000);
         }
 
         public static int SwapEndian(int input)
         {
-            uint inputAsUint = (uint)input;
+            var inputAsUint = (uint) input;
 
             input = (int)
-                    (((inputAsUint >> 24) & 0xff) |
-                    ((inputAsUint << 8) & 0xff0000) |
-                    ((inputAsUint >> 8) & 0xff00) |
-                    ((inputAsUint << 24) & 0xff000000));
+                (((inputAsUint >> 24) & 0xff) |
+                 ((inputAsUint << 8) & 0xff0000) |
+                 ((inputAsUint >> 8) & 0xff00) |
+                 ((inputAsUint << 24) & 0xff000000));
 
             return input;
         }
@@ -145,15 +143,15 @@ namespace FFXIVClassic.Common
             // 'm' and 'r' are mixing constants generated offline.
             // They're not really 'magic', they just happen to work well.
 
-            byte[] data = Encoding.ASCII.GetBytes(key);
+            var data = Encoding.ASCII.GetBytes(key);
             const uint m = 0x5bd1e995;
             const int r = 24;
-            int len = key.Length;
-            int dataIndex = len - 4;
+            var len = key.Length;
+            var dataIndex = len - 4;
 
             // Initialize the hash to a 'random' value
 
-            uint h = seed ^ (uint)len;
+            var h = seed ^ (uint) len;
 
             // Mix 4 bytes at a time into the hash
 
@@ -162,11 +160,11 @@ namespace FFXIVClassic.Common
             {
                 h *= m;
 
-                uint k = (uint)BitConverter.ToInt32(data, dataIndex);
+                var k = (uint) BitConverter.ToInt32(data, dataIndex);
                 k = ((k >> 24) & 0xff) | // move byte 3 to byte 0
-                        ((k << 8) & 0xff0000) | // move byte 1 to byte 2
-                        ((k >> 8) & 0xff00) | // move byte 2 to byte 1
-                        ((k << 24) & 0xff000000); // byte 0 to byte 3
+                    ((k << 8) & 0xff0000) | // move byte 1 to byte 2
+                    ((k >> 8) & 0xff00) | // move byte 2 to byte 1
+                    ((k << 24) & 0xff000000); // byte 0 to byte 3
 
                 k *= m;
                 k ^= k >> r;
@@ -182,14 +180,17 @@ namespace FFXIVClassic.Common
             switch (len)
             {
                 case 3:
-                    h ^= (uint)data[0] << 16; goto case 2;
+                    h ^= (uint) data[0] << 16;
+                    goto case 2;
                 case 2:
-                    h ^= (uint)data[len - 2] << 8; goto case 1;
+                    h ^= (uint) data[len - 2] << 8;
+                    goto case 1;
                 case 1:
                     h ^= data[len - 1];
                     h *= m;
                     break;
-            };
+            }
+            ;
 
             // Do a few final mixes of the hash to ensure the last few
             // bytes are well-incorporated.
@@ -203,16 +204,16 @@ namespace FFXIVClassic.Common
 
         public static byte[] ConvertBoolArrayToBinaryStream(bool[] array)
         {
-            byte[] data = new byte[(array.Length / 8) + (array.Length % 8 != 0 ? 1 : 0)];
+            var data = new byte[array.Length/8 + (array.Length%8 != 0 ? 1 : 0)];
 
-            int dataCounter = 0;
-            for (int i = 0; i < array.Length; i += 8)
+            var dataCounter = 0;
+            for (var i = 0; i < array.Length; i += 8)
             {
-                for (int bitCount = 0; bitCount < 8; bitCount++)
+                for (var bitCount = 0; bitCount < 8; bitCount++)
                 {
                     if (i + bitCount >= array.Length)
                         break;
-                    data[dataCounter] = (byte)(((array[i + bitCount] ? 1 : 0) << 7 - bitCount) | data[dataCounter]);
+                    data[dataCounter] = (byte) (((array[i + bitCount] ? 1 : 0) << 7 - bitCount) | data[dataCounter]);
                 }
                 dataCounter++;
             }
@@ -222,10 +223,10 @@ namespace FFXIVClassic.Common
 
         public static string ToStringBase63(int number)
         {
-            string lookup = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var lookup = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-            string secondDigit = lookup.Substring((int)Math.Floor((double)number / (double)lookup.Length), 1);
-            string firstDigit = lookup.Substring(number % lookup.Length, 1);
+            var secondDigit = lookup.Substring((int) Math.Floor(number/(double) lookup.Length), 1);
+            var firstDigit = lookup.Substring(number%lookup.Length, 1);
 
             return secondDigit + firstDigit;
         }
--- FFXIVClassic Map Server/packets/BasePacket.cs
+++ FFXIVClassic Map Server/packets/BasePacket.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.InteropServices;
 using FFXIVClassic.Common;
 using NLog;
 using NLog.Targets;
