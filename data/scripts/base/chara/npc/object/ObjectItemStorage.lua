c4c43421f3e5b6cdecb16a5713ef76347bd3fbea
Filip Maj <filipmaj@gmail.com>
2016-08-05 18:24:17 -0400

    Forgot ObjectItemStorage script.

--- data/scripts/base/chara/npc/object/ObjectItemStorage.lua
+++ data/scripts/base/chara/npc/object/ObjectItemStorage.lua
@@ -1,3 +1,15 @@
+--[[
+
+ObjectItemStorage Script
+
+Functions:
+
+storageMenu() - Shows store/retrieve/help menu.
+selectCategory() - Shows the category menu
+selectStoreItem(nil, categoryId) - Shows store item menu
+selectReceiveItem(nil, categoryId) - Shows retrieve item menu
+--]]
+
 require ("global")
 
 function init(npc)
@@ -6,6 +18,29 @@ end
 
 function onEventStarted(player, npc, triggerName)	
 	
+	::TOP_MENU::
+	storageChoice = callClientFunction(player, "storageMenu");
+	
+	if (storageChoice == 1) then
+		categoryChoice = callClientFunction(player, "selectCategory");
+		
+		if (categoryChoice == 5) then
+			goto TOP_MENU;
+		end
+		
+		callClientFunction(player, "selectStoreItem", nil, categoryChoice);
+		
+	elseif (storageChoice == 2) then
+		categoryChoice = callClientFunction(player, "selectCategory");
+	
+		if (categoryChoice == 5) then
+			goto TOP_MENU;
+		end
+	
+		callClientFunction(player, "selectReceiveItem", nil, categoryChoice);
+	
+	end
+	
 	player:EndEvent();
 	
-end
+end 
