d6277bc7222bf72ee72c47d7840d33a7e8c17b53
Filip Maj <filipmaj@gmail.com>
2016-08-21 18:18:11 -0400

    Sql update for the last commit.

--- sql/supportdesk_tickets.sql
+++ sql/supportdesk_tickets.sql
@@ -4,7 +4,7 @@ Source Host: localhost
 Source Database: ffxiv_server
 Target Host: localhost
 Target Database: ffxiv_server
-Date: 8/20/2016 7:15:46 PM
+Date: 8/21/2016 6:17:47 PM
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -13,11 +13,13 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 CREATE TABLE `supportdesk_tickets` (
   `id` int(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL,
   `title` varchar(128) NOT NULL,
   `body` text NOT NULL,
   `langCode` smallint(4) unsigned NOT NULL,
+  `isOpen` tinyint(1) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records 
