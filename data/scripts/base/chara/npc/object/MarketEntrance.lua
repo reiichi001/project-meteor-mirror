6e7459a2f850886aeca7b9c3be856ee7bab99b2f
dude22072 <dude2207@gmail.com>
2016-10-04 06:03:33 +0000

    MarketEntrance.lua syntax error

--- data/scripts/base/chara/npc/object/MarketEntrance.lua
+++ data/scripts/base/chara/npc/object/MarketEntrance.lua
@@ -16,7 +16,7 @@ function init(npc)
 end
 
 function onEventStarted(player, npc, triggerName)	
-	callClientFunction(player, "eventPushChoiceAreaOrQuest", 0xc13, 0xc1a, 0xdba,0, false, 0));
+	callClientFunction(player, "eventPushChoiceAreaOrQuest", 0xc13, 0xc1a, 0xdba,0, false, 0);
 		
 	player:EndEvent();	
 end
