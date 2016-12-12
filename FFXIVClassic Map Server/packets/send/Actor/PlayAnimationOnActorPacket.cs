09e1e31e7948d647761d2e4590e39dfdcecf15ca
Filip Maj <filipmaj@gmail.com>
2016-12-12 10:11:27 -0500

    Added packet 0xda

---
+++ FFXIVClassic Map Server/packets/send/Actor/PlayAnimationOnActorPacket.cs
@@ -0,0 +1,20 @@
+﻿using FFXIVClassic.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFXIVClassic_Map_Server.packets.send.actor
+{
+    class PlayAnimationOnActorPacket
+    {
+        public const ushort OPCODE = 0x00DA;
+        public const uint PACKET_SIZE = 0x28;
+
+        public static SubPacket BuildPacket(uint playerActorID, uint targetActorID, uint animationID)
+        {
+            return new SubPacket(OPCODE, playerActorID, targetActorID, BitConverter.GetBytes((ulong)animationID));
+        }
+    }
+}
