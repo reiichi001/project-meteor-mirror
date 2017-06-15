cdfa2f682e7f0eaae21e59c18de77f295b76bbbb
Filip Maj <filipmaj@gmail.com>
2017-06-14 22:41:39 -0400

    Fixed the broken elevator in Ul'dah. Finished the one in Limsa.

--- data/scripts/unique/sea0Town01/ElevatorStandard/crows_lift_lvl1.lua
+++ data/scripts/unique/sea0Town01/ElevatorStandard/crows_lift_lvl1.lua
@@ -4,9 +4,11 @@ function onEventStarted(player, npc)
 	floorChoice = callClientFunction(player, "elevatorAskLimsa001", 0);	
 	
 	if (floorChoice == 1) then
-		callClientFunction(player, "elevatorAskLimsa001", 1);				
+		callClientFunction(player, "elevatorAskLimsa001", 1);
+		GetWorldManager():DoZoneChange(player, 133, nil, 0, 15, -447, 40, 220, -1.574);	
 	elseif (floorChoice == 2) then
-		callClientFunction(player, "elevatorAskLimsa001", 2);		
+		callClientFunction(player, "elevatorAskLimsa001", 2);	
+		GetWorldManager():DoZoneChange(player, 133, nil, 0, 15, -458, 92, 175, -0.383);	
 	end
 	
 	player:EndEvent();	
--- data/scripts/unique/sea0Town01/ElevatorStandard/crows_lift_lvl3.lua
+++ data/scripts/unique/sea0Town01/ElevatorStandard/crows_lift_lvl3.lua
@@ -4,9 +4,11 @@ function onEventStarted(player, npc)
 	floorChoice = callClientFunction(player, "elevatorAskLimsa003", 0);	
 	
 	if (floorChoice == 1) then
-		callClientFunction(player, "elevatorAskLimsa003", 1);		
+		callClientFunction(player, "elevatorAskLimsa003", 1);
+		GetWorldManager():DoZoneChange(player, 133, nil, 0, 15, -447, 19, 220, -1.574);
 	elseif (floorChoice == 2) then
 		callClientFunction(player, "elevatorAskLimsa003", 2);
+		GetWorldManager():DoZoneChange(player, 133, nil, 0, 15, -447, 40, 220, -1.574);
 	end
 	
 	player:EndEvent();	
