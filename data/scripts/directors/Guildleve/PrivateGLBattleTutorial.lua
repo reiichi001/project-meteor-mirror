c42f1a08de098c20e9777ba275588c454ee90ca3
Filip Maj <filipmaj@gmail.com>
2017-06-24 17:18:28 -0400

    Changed to Tutorial GL script as that is the correct one for our test GL.

---
+++ data/scripts/directors/Guildleve/PrivateGLBattleTutorial.lua
@@ -0,0 +1,25 @@
+require ("global")
+require ("guildleve")
+
+--DirectorId, GuildleveId, Aetheryte Location (6 or ~6), exMarkerX, exMarkerY, exMarkerZ
+
+function init(thisDirector)
+	guildleveData = GetGuildleveGamedata(thisDirector.guildleveId);
+	members = thisDirector:GetPlayerMembers();
+	
+	if (members ~= nil and #members ~= 0) then
+		player = members[0];
+		player:SendGameMessage(GetWorldMaster(), 50036, 0x20, thisDirector.guildleveId, player, 0); --"You have started the leve..."
+		player:PlayAnimation(getGLStartAnimationFromSheet(guildleveData.borderId, guildleveData.plateId, false));
+	end
+	
+	return "/Director/Guildleve/PrivateGLBattleTutorial", 0x4e25, thisDirector.guildleveId, 6, 0, 0, 0;
+end
+
+function mainLoop(thisDirector)
+
+	wait(2)
+	thisDirector:StartGuildleve();
+	thisDirector:SyncAllInfo();
+	
+end
