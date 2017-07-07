247b5ca7095e9f88b1b99b80d390fa1c11e0ebed
Tahir Akhlaq <xdemolish@hotmail.com>
2017-07-08 00:20:55 +0100

    missed some files from previous commit

---
+++ FFXIVClassic Map Server/actors/chara/ai/controllers/BattleNpcController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FFXIVClassic_Map_Server.Actors;
+
+namespace FFXIVClassic_Map_Server.actors.chara.ai.controllers
+{
+    class BattleNpcController : Controller
+    {
+        public BattleNpcController(Character owner)
+        {
+            this.owner = owner;
+            this.lastUpdate = DateTime.Now;
+        }
+
+        public override void Update(DateTime tick)
+        {
+            // todo: handle aggro/deaggro and other shit here
+            ((BattleNpc)this.owner).statusEffects.Update(tick);
+        }
+
+        public override bool Engage(Character target)
+        {
+            // todo: check distance, last swing time, status effects
+            this.owner.aiContainer.InternalEngage(target);
+            return true;
+        }
+
+        private bool TryEngage(Character target)
+        {
+            // todo:
+            return true;
+        }
+
+        public override bool Disengage()
+        {
+            // todo:
+            return true;
+        }
+
+        public override void Cast(Character target, uint spellId)
+        {
+
+        }
+
+        public override void Ability(Character target, uint abilityId)
+        {
+
+        }
+
+        public override void RangedAttack(Character target)
+        {
+
+        }
+
+        public override void MobSkill(Character target, uint mobSkillId)
+        {
+            
+        }
+    }
+}
---
+++ FFXIVClassic Map Server/actors/chara/npc/Pet.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FFXIVClassic_Map_Server.actors.chara.ai;
+using FFXIVClassic_Map_Server.actors.chara.ai.controllers;
+using FFXIVClassic_Map_Server.actors.chara.npc;
+using FFXIVClassic_Map_Server.packets.send.actor;
+
+namespace FFXIVClassic_Map_Server.Actors
+{
+    class Pet : BattleNpc
+    {
+        public Pet(int actorNumber, ActorClass actorClass, string uniqueId, Area spawnedArea, float posX, float posY, float posZ, float rot,
+                    ushort actorState, uint animationId, string customDisplayName)
+            : base(actorNumber, actorClass, uniqueId, spawnedArea, posX, posY, posZ, rot, actorState, animationId, customDisplayName)  
+        {
+            this.aiContainer = new AIContainer(this, new BattleNpcController(this), new PathFind(this), new TargetFind(this));
+            this.currentSubState = SetActorStatePacket.SUB_STATE_MONSTER;
+            this.hateContainer = new HateContainer(this);
+        }
+    }
+}
