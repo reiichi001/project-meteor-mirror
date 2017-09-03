4e39b082f1e47f729164ac4cc5606febf552cc28
Filip Maj <filipmaj@gmail.com>
2017-09-03 16:44:10 -0400

    Added new scripts, implemented some old ones.

--- data/scripts/base/chara/npc/populace/PopulaceItemRepairer.lua
+++ data/scripts/base/chara/npc/populace/PopulaceItemRepairer.lua
@@ -4,8 +4,8 @@ PopulaceItemRepairer Script
 
 Functions:
 
-talkWelcome(player, bool, number, bool) - Opens the main menu
-selectItem(nil, pageNumber, ?, condition1, condition2, condition3, condition4, condition5) - "Ain't running a charity here", message said when you have insufficent funds
+talkWelcome(player, sayWelcomeText, currentLevel?, changes 1500243 from "welcome" to "well met") - Opens the main menu
+selectItem(nil, pageNumber, ?, condition1, condition2, condition3, condition4, condition5) - Select item slot.
 confirmRepairItem(player, price, itemId, hq grade) - Shows the confirm box for item repair.
 confirmUseFacility(player, price) - Shows confirm box for using facility. Default price is 11k?
 finishTalkTurn() - Call at end to stop npc from staring at the player (eeeek)
@@ -18,12 +18,29 @@ function init(npc)
 	return false, false, 0, 0;	
 end
 
-function onEventStarted(player, npc, triggerName)
+function onEventStarted(player, npc, triggerName)	
 	
-	result = callClientFunction(player, "talkWelcome", player, false, 20, false);
+	result = callClientFunction(player, "talkWelcome", player, true, 20, false);
 	
 	if (result == 1) then	
-		callClientFunction(player, "selectItem", nil, 1, 4, 2, 3, 4, 5, 6, 7);
+		local currentPage = 1;
+		local slotToRepair = nil;
+		
+		while (true) do
+			slot, page, listIndx = callClientFunction(player, "selectItem", nil, currentPage, 4, 2, 55, 55, 55, 55);
+			
+			if (slot == nil and page ~= nil) then
+				currentPage = page;				
+			else
+				slotToRepair = slot;
+				break;
+			end			
+		end
+		
+		if (slotToRepair ~= nil) then
+			callClientFunction(player, "confirmRepairItem", player, 100, 8032827, 0);
+		end
+		
 	elseif (result == 2) then	
 		callClientFunction(player, "confirmUseFacility", player);
 	end
