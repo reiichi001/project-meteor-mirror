8f487689ba0714e5cb9008b1e601edbfbf4f3228
Filip Maj <filipmaj@gmail.com>
2017-09-09 14:32:08 -0400

    Pushed new db schema for inventory and retainers.

--- sql/server_items.sql
+++ sql/server_items.sql
@@ -1,27 +1,16 @@
--- MySQL dump 10.13  Distrib 5.7.10, for Win64 (x86_64)
---
--- Host: localhost    Database: ffxiv_database
--- ------------------------------------------------------
--- Server version	5.7.10-log
+/*
+MySQL Data Transfer
+Source Host: localhost
+Source Database: ffxiv_server
+Target Host: localhost
+Target Database: ffxiv_server
+Date: 9/9/2017 2:31:34 PM
+*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `server_items`
---
-
-DROP TABLE IF EXISTS `server_items`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+SET FOREIGN_KEY_CHECKS=0;
+-- ----------------------------
+-- Table structure for server_items
+-- ----------------------------
 CREATE TABLE `server_items` (
   `id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `itemId` int(10) unsigned NOT NULL,
@@ -35,25 +24,4 @@ CREATE TABLE `server_items` (
   `materia4` tinyint(3) unsigned DEFAULT '0',
   `materia5` tinyint(3) unsigned DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `server_items`
---
-
-LOCK TABLES `server_items` WRITE;
-/*!40000 ALTER TABLE `server_items` DISABLE KEYS */;
-/*!40000 ALTER TABLE `server_items` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-06-07 22:54:54
+) ENGINE=InnoDB AUTO_INCREMENT=1435 DEFAULT CHARSET=utf8;
