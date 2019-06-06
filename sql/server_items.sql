fea8638775518af395b287bc27f5a0f4f45ef446
Filip Maj <filipmaj@gmail.com>
2019-06-06 01:50:15 -0400

    Changed id datatype for items to bigint. Removed bazaar and retainer inventories (now all in the characters_inventory table). Added server_items_dealing table for bazaar.

--- sql/server_items.sql
+++ sql/server_items.sql
@@ -1,27 +1,25 @@
-/*
-MySQL Data Transfer
-Source Host: localhost
-Source Database: ffxiv_server
-Target Host: localhost
-Target Database: ffxiv_server
-Date: 12/17/2017 3:38:35 PM
-*/
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.17 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.1.0.5464
+-- --------------------------------------------------------
 
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for server_items
--- ----------------------------
-CREATE TABLE `server_items` (
-  `id` bigint(20) unsigned zerofill NOT NULL AUTO_INCREMENT,
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table ffxiv_server.server_items
+CREATE TABLE IF NOT EXISTS `server_items` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `itemId` int(10) unsigned NOT NULL,
-  `quantity` int(10) unsigned DEFAULT '1',
-  `quality` tinyint(3) unsigned DEFAULT '1',
-  `isExclusive` tinyint(1) unsigned DEFAULT '0',
-  `isAttached` tinyint(1) unsigned DEFAULT '0',
-  `isDealing` tinyint(1) unsigned DEFAULT '0',
+  `quantity` int(10) unsigned NOT NULL DEFAULT '0',
+  `quality` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=150 DEFAULT CHARSET=utf8;
 
--- ----------------------------
--- Records 
--- ----------------------------
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- sql/server_items_modifiers.sql
+++ sql/server_items_modifiers.sql
@@ -1,26 +1,27 @@
-/*
-MySQL Data Transfer
-Source Host: localhost
-Source Database: ffxiv_server
-Target Host: localhost
-Target Database: ffxiv_server
-Date: 12/17/2017 3:38:44 PM
-*/
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.6.17 - MySQL Community Server (GPL)
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.1.0.5464
+-- --------------------------------------------------------
 
-SET FOREIGN_KEY_CHECKS=0;
--- ----------------------------
--- Table structure for server_items_modifiers
--- ----------------------------
-CREATE TABLE `server_items_modifiers` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `durability` int(10) unsigned NOT NULL DEFAULT '0',
-  `mainQuality` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `subQuality1` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `subQuality2` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `subQuality3` tinyint(3) unsigned NOT NULL DEFAULT '1',
-  `param1` int(10) unsigned NOT NULL DEFAULT '0',
-  `param2` int(10) unsigned NOT NULL DEFAULT '0',
-  `param3` int(10) unsigned NOT NULL DEFAULT '0',
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+-- Dumping structure for table ffxiv_server.server_items_modifiers
+CREATE TABLE IF NOT EXISTS `server_items_modifiers` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `durability` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `mainQuality` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `subQuality1` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `subQuality2` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `subQuality3` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `param1` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `param2` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `param3` mediumint(8) unsigned NOT NULL DEFAULT '0',
   `spiritbind` smallint(5) unsigned NOT NULL DEFAULT '0',
   `materia1` smallint(5) unsigned NOT NULL DEFAULT '0',
   `materia2` smallint(5) unsigned NOT NULL DEFAULT '0',
@@ -28,8 +29,8 @@ CREATE TABLE `server_items_modifiers` (
   `materia4` smallint(5) unsigned NOT NULL DEFAULT '0',
   `materia5` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2315 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=142 DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Records 
--- ----------------------------
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
