9fd0e96b0e1453db1ae041590d12b1a83306f495
Filip Maj <filipmaj@gmail.com>
2019-06-17 23:21:36 -0400

    Added AGPL license to the whole codebase. All other devs who's code is in the project as of June 2019 came to agreement that they were fine to add this license to the project; Demo, Devi, Jorge, Azia, Shozokui, Dude22072.

--- FFXIVClassic Map Server/dataobjects/GuildleveData.cs
+++ FFXIVClassic Map Server/dataobjects/GuildleveData.cs
@@ -1,4 +1,25 @@
-﻿using MySql.Data.MySqlClient;
+﻿/*
+===========================================================================
+Copyright (C) 2015-2019 FFXIV Classic Server Dev Team
+
+This file is part of FFXIV Classic Server.
+
+FFXIV Classic Server is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FFXIV Classic Server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with FFXIV Classic Server. If not, see <https:www.gnu.org/licenses/>.
+===========================================================================
+*/
+
+using MySql.Data.MySqlClient;
 
 namespace FFXIVClassic_Map_Server.dataobjects
 {
