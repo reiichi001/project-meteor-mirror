8940e65885182fac8c032cd915bc4a2fbcd26dbb
Filip Maj <filipmaj@gmail.com>
2019-06-18 22:55:32 -0400

    Changed the project name from "FFXIV Classic Server" to "Project Meteor Server" to avoid trademark issues.

--- FFXIVClassic Map Server/actors/chara/ai/AIContainer.cs
+++ FFXIVClassic Map Server/actors/chara/ai/AIContainer.cs
@@ -1,4 +1,25 @@
-﻿using System;
+﻿/*
+===========================================================================
+Copyright (C) 2015-2019 Project Meteor Dev Team
+
+This file is part of Project Meteor Server.
+
+Project Meteor Server is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Project Meteor Server is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Project Meteor Server. If not, see <https:www.gnu.org/licenses/>.
+===========================================================================
+*/
+
+using System;
 using System.Collections.Generic;
 using FFXIVClassic_Map_Server.Actors;
 using FFXIVClassic_Map_Server.actors.chara.ai.state;
